4a7f6a08a5220b64c1093ffbdae9b8416cf09ea3
Hui Jun Yang <heejun1481@jbnu.ac.kr>
2024-10-24 15:24:08 +0900

    Pandas 문제 해결 완료

--- HW08/hw08_bootstrap.py
+++ HW08/hw08_bootstrap.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, render_template
 #import pandas as pd
 
@@ -19,8 +20,9 @@ def edu():
 def map():
     return render_template('map.html')
 
-@app.route('/blog')
+""""@app.route('/blog')
 def blog():
+
     posts = [
         {'title': '첫 번째 글', 'content': '이것은 첫 번째 블로그 글입니다.'},
         {'title': '두 번째 글', 'content': '이것은 두 번째 블로그 글입니다.'},
@@ -30,17 +32,18 @@ def blog():
         {'title': '여섯 번째 글', 'content': '이것은 여섯 번째 블로그 글입니다.'},
         {'title': '일곱 번째 글', 'content': '이것은 일곱 번째 블로그 글입니다.'},
     ]
-    return render_template('blog.html', posts=posts)
+    return render_template('blog.html', posts=posts)"""
     #return render_template('blog.html', title="blog post", posts=posts)  #페이지마다 제목 변경하려면 title이라는 변수 사용
 
 # <csv파일에 13개의 데이터를 작성해둠. 불러오려 했는데 실패. ParserError뜨면서 blog.html 화면 안열림.>
-"""@app.route('/blog')
+@app.route('/blog')
 def blog():
-    df = pd.read_csv('blog_content.csv', encoding='utf-8')  
+    df = pd.read_csv('static/blog_content.csv', encoding='UTF-8')
+    print(df.columns)
     posts = []
     for i, row in df.iterrows():
         posts.append({'title': row['title'], 'content': row['content']})
-    return render_template('blog.html', posts=posts)"""
+    return render_template('blog.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
