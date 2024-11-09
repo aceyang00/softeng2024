e83f226ebaa7a2f4418756f9b4d43803ca4beca8
HuiJun Yang <117896951+aceyang00@users.noreply.github.com>
2024-11-09 22:26:42 +0900

    Delete HW11 directory

--- HW11/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PostList.as_view(), name='blog'),  # 이 URL에 'blog'라는 이름을 추가
-    path('<int:pk>/', views.PostDetail.as_view(), name='post_detail')  # 필요에 따라 이름을 추가
-]
